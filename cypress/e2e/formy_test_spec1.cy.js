ada539f67b9d92ee7a0fb619f3bbe70e8a27201b
cinematografie <kristinadruin@gmail.com>
2023-03-19 18:18:04 +0000

    kd - add checkbox tests and cy scripts

--- cypress/e2e/formy_test_spec1.cy.js
+++ cypress/e2e/formy_test_spec1.cy.js
@@ -8,36 +8,43 @@ describe('formy test spec', () => {
 
   //not testing Autocomplete due to Places API error: BillingNotEnabledMapError on Formy site
   it('Formy site - Checkbox tests', () => {
+    //Asserting we made it to the right page
     cy.get('.jumbotron-fluid').contains('Checkbox').click()
         .waitUntil(() => cy.get('h1').contains('Checkboxes'))
-    //add clicks for checkboxes
-  })
+    cy.get('#checkbox-1').click()
+        .should('be.checked')
+    cy.get('#checkbox-2').click()
+        .should('be.checked')
+    cy.get('#checkbox-3').click()
+        .should('be.checked')
+      })
+
 
   it('Formy site - Datepicker tests', () => {
   })
 
-  //note that drag and drop on formy does not work on all browsers (chrome only)
-  it('Formy site - Drag and drop tests', () => {
-  })
-
-  it('Formy site - Enabled and Disabled Elements tests', () => {
-  })
-
-  it('Formy site - Modal tests', () => {
-
-  })
-
-  it('Formy site - Page Scroll tests', () => {
-
-  })
-
-  it('Formy site - Switch Window tests', () => {
-
-  })
-
-  //covers multiple of the categories from picker
-  it('Formy site - Complete Web Form tests', () => {
-
-  })
-
+  // //note that drag and drop on formy does not work on all browsers (chrome only)
+  // it('Formy site - Drag and drop tests', () => {
+  // })
+  //
+  // it('Formy site - Enabled and Disabled Elements tests', () => {
+  // })
+  //
+  // it('Formy site - Modal tests', () => {
+  //
+  // })
+  //
+  // it('Formy site - Page Scroll tests', () => {
+  //
+  // })
+  //
+  // it('Formy site - Switch Window tests', () => {
+  //
+  // })
+  //
+  // //covers multiple of the categories from picker
+  // it('Formy site - Complete Web Form tests', () => {
+  //
+  // })
+  //
 })
